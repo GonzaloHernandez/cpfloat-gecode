ca455dac7f99250bbbedd96b1426b2b04ba23bd1
GonzaloHernández <gonzalohernandez@udenar.edu.co>
2022-04-04 11:36:46 -0500

    Copyright texts

--- cpfloat/prop/k3b.cpp
+++ cpfloat/prop/k3b.cpp
@@ -1,3 +1,25 @@
+/*
+ *  Authors:
+ *     Gonzalo Hernández <gonzalohernandez@udenar.edu.co>
+ *     Gustavo Gutierrez
+ *
+ *  Year of last major update
+ *     2011
+ * 
+ *  This file is a complement of Gecode, the generic constraint
+ *  development environment:
+ *     http://www.gecode.org
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ *  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ *  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ *  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
 #include <cpfloat/prop/k3b.hh>
 
 namespace MPG {
